1bb3fd19d40623d9e33af6b8675ef409e886ed4e
Delta-Hat <sdhmob@gmail.com>
2023-08-05 17:28:34 -0400

    added budget stuff

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,10 +2,39 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { dispatch,budget,expenses } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);//taken from ExpenseTotal.js
+    const updateBudget = (newValue) => {
+        console.log('Budget: ' + budget + '\n' +
+        'ExpensesTotal: ' + totalExpenses+ '\n' +
+        'NewValue: ' + newValue);
+        if(newValue > 20000){
+            alert("Budget cannot exceed £20000");
+            return;
+        }
+        if(newValue < totalExpenses){
+            alert("Budget cannot be lower than expenses.")
+            return;
+        }
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: newValue
+        })
+    }
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £
+                <input
+                    type='number'
+                    id='budget'
+                    value={budget}
+                    step='10'
+                    onChange={(event) => updateBudget(event.target.value)}
+                >
+                </input>
+            </span>
         </div>
     );
 };
